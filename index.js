bac8a960383fdb43a8ba107da3d388d4432d40c3
Kaique Silva <kaeyosthaeron@gmail.com>
2020-07-10 20:31:37 -0300

    Expading template syntax

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 'use strict';
 const assert = require('assert');
 
-function render(expression, context) {
+function replace(expression, context) {
+  const self = this;
+  let out = '';
+  self.matches.forEach(matched => {
+    out = expression.replace(self.key, context[self.key]);
+  });
+  return out;
+}
+
+function traverse(expression, context) {
   let out = '';
   Object.keys(context).forEach(key => {
     let matches = [...expression.matchAll(key)];
-    matches.forEach(matched => {
-      out = expression.replace(key, context[key]);
-    });
+    out = replace.call({ matches: matches, key: key }, expression, context);
   });
-  out = out.replace('{{', '');
-  out = out.replace('}}', '');
   return out;
 }
-assert.equal(render('{{message}}', {message: 'hello world'}), 'hello world');
+
+function render(expression, context) {
+  let out = expression;
+  let rTemplate = new RegExp('(\{\{.*?\}\})', 'ig');
+  let expressions = [...expression.matchAll(rTemplate)];
+  expressions.forEach(v => {
+    out = traverse(out, context);
+    out = out.replace('{{', '');
+    out = out.replace('}}', '');
+  });
+  return out;
+}
+assert.equal(render('{{message}}', { message: 'hello world' }), 'hello world');
+assert.equal(render('{{message}} {{message}}', {
+  message: 'hello world'
+}), 'hello world hello world');
