db855dec1bd6b2c618f0659a0eec41f70e9f71bd
Kaique Silva <kaeyosthaeron@gmail.com>
2020-07-11 16:30:36 -0300

    Expanding template syntax
    Implemented the replacement for specific cases

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const assert = require('assert');
 
+
 function replace(expression, context) {
   const self = this;
   let out = '';
@@ -19,22 +20,30 @@ function traverse(expression, context) {
   return out;
 }
 
+const rTemplate = new RegExp('(\{\{.*?\}\})', 'ig');
+
+function hasFragments(expression) {
+  return [...expression.matchAll(rTemplate)].length > 0;
+}
+
 function render(expression, context) {
   let out = expression;
-  let rTemplate = new RegExp('(\{\{.*?\}\})', 'ig');
-  let expressions = [...expression.matchAll(rTemplate)];
-  expressions.forEach(v => {
-    out = traverse(out, context);
+  let expressions = [...out.matchAll(rTemplate)];
+  let c = 0;
+  for (let i = 0; i < expressions.length; i += 1) {
+    if (hasFragments(out) && traverse(out, context)) {
+      out = traverse(out, context);
+    }
     out = out.replace('{{', '');
     out = out.replace('}}', '');
-  });
+  }
   return out;
 }
 assert.equal(render('{{message}}', { message: 'hello world' }), 'hello world');
 assert.equal(render('{{message}} {{message}}', {
   message: 'hello world'
 }), 'hello world hello world');
-assert.equal(render('{{firstValue + secondValue}}', {
+assert.equal(render('{{firstValue}} + {{secondValue}}', {
   firstValue: 2,
   secondValue: 3,
 }), '2 + 3');
